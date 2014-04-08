b407b993aa1d3a70fc81a94a5ba4a88fbd8fcc70
roddog <rodney.dyer@gmail.com>
2014-04-08 13:16:47 -0400

    Changed theme_empty() to allow the showing of a legend IF there is a need for a legend (like using gradients for edge weights).

--- R/theme_empty.R
+++ R/theme_empty.R
@@ -19,42 +19,34 @@
 #' p <- p + geom_nodeset( aes(x,y), lopho )
 #' p 
 #' p + theme_empty()
+#' p <- ggplot() + geom_edgeset( aes(x,y,color=weight), lopho)
+#' p
 #' 
 theme_empty <- function (base_size = 12, base_family = "Helvetica"){
-  theme(
-    line = element_blank(), 
-    rect = element_blank(), 
-    text = element_blank(),
-    axis.line = element_blank(), 
-    axis.text = element_blank(),
-    axis.ticks = element_blank(),
-    axis.title = element_blank(), 
-    axis.title.y = element_text(angle = 90, vjust = 0.5), 
-    axis.ticks.length = grid::unit(0.3, "lines"), 
-    axis.ticks.margin = grid::unit(0.5, "lines"), 
-    legend.background = element_rect(colour = NA), 
-    legend.key = element_blank(), 
-    legend.key.size = grid::unit(1.2, "lines"), 
-    legend.key.height = NULL, 
-    legend.key.width = NULL,     
-    legend.text = element_blank(), 
-    legend.title = element_blank(),
-    legend.position = "right", 
-    legend.text.align = NULL, 
-    legend.title.align = NULL, 
-    legend.direction = "vertical", 
-    legend.box = NULL,    
-    panel.background = element_rect(fill = "transparent", colour = NA), 
-    panel.border = element_blank(), 
-    panel.grid.major = element_blank(), 
-    panel.grid.minor = element_blank(), 
-    panel.margin = grid::unit(0.25, "lines"), 
-    strip.background = element_blank(), 
-    strip.text = element_blank(),
-    strip.text.y = element_text(angle = -90), 
-    plot.background = element_rect(colour = 'NA', fill = 'transparent'), 
-    plot.title = element_text(size = base_size * 1.2), 
-    plot.margin = grid::unit(c(1, 1, 0.5, 0.5), "lines"),
-    
-    complete = TRUE)   # denotes that this is a complete theme function
+    theme(
+      line = element_blank(), 
+      rect = element_blank(), 
+      axis.line = element_blank(), 
+      axis.text = element_blank(),
+      axis.ticks = element_blank(),
+      axis.title = element_blank(), 
+      axis.title.y = element_text(angle = 90, vjust = 0.5), 
+      axis.ticks.length = grid::unit(0.3, "lines"), 
+      axis.ticks.margin = grid::unit(0.5, "lines"),   
+      panel.background = element_rect(fill = "transparent", colour = NA), 
+      panel.border = element_blank(), 
+      panel.grid.major = element_blank(), 
+      panel.grid.minor = element_blank(), 
+      panel.margin = grid::unit(0.25, "lines"), 
+      strip.background = element_blank(), 
+      strip.text = element_blank(),
+      strip.text.y = element_text(angle = -90), 
+      plot.background = element_rect(colour = 'NA', fill = 'transparent'), 
+      plot.title = element_text(size = base_size * 1.2), 
+      plot.margin = grid::unit(c(1, 1, 0.5, 0.5), "lines"),   
+      complete = TRUE)   # denotes that this is a complete theme function
 }
+
+
+
+
